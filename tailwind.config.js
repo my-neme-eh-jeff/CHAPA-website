f30c577711e19fd53166db826c4b35020a6a7cfe
Aman <aman2003nambisan@gmail.com>
2024-06-14 09:44:03 +0530

    fix: removes type annotaions from tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
@@ -116,6 +115,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+} 
 
 export default config;
